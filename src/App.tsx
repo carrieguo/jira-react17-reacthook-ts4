dee4c911e443493b06297fe491e32d2d3b5cdb80
carrieguo <593774767@qq.com>
2023-01-08 18:37:22 +0800

    sss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
@@ -9,12 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-
-
-
-
-
-          
         </p>
         <a
           className="App-link"
