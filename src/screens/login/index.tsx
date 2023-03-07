0851933d53930f43ab3d4bf47b7fe69ada7d06f0
Guo Xiaohui 郭晓慧 <guoxh@haier.com>
2023-03-07 17:53:16 +0800

    feat: 第五章

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,19 +1,7 @@
 import React, { FormEvent } from 'react'
 const apiUrl = process.env.REACT_APP_API_URL
 export const LoginScreen = () => {
-  const login = (param: { username: string; password: string }) => {
-    fetch(`${apiUrl}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(param),
-    }).then(async (response) => {
-      if (response.ok) {
-        console.log(response)
-      }
-    })
-  }
+  const login = (param: { username: string; password: string }) => {}
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const username = (event.currentTarget.elements[0] as HTMLInputElement).value
@@ -30,7 +18,7 @@ export const LoginScreen = () => {
         <label htmlFor="password">密码</label>
         <input type="password" id={'password'} />
       </div>
-      <button type={'submit'}>登录</button>
+      <button type={'submit'}>注册</button>
     </form>
   )
 }
